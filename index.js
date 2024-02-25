ef0bb866e96b6fe9735ef2321a3edf6001c974f5
Dobataa <dobatabg04@gmail.com>
2024-02-25 22:55:12 +0200

    add router middleware

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const express = require('express');
 const config = require('./config/config');
 const app = express();
 const expressConfig = require('./config/express');
+const routes = require('./routes');
 
 expressConfig(app);
-
-app.get('/', (req, res) => {
-    res.render('home', {layout:false});
-});
-
+app.use(routes);
 
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}..`));
