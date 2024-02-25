b476d215068f94e8423031997f52806d262ab97e
Dobataa <dobatabg04@gmail.com>
2024-02-25 22:40:23 +0200

    add view engine

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const config = require('./config/config');
 
 const app = express();
+
+app.engine('.hbs', engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {layout:false});
+});
 
 
 
