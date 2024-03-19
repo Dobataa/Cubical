e4a43f027114225cdcbcf45ca285a3f9f9be2f4f
Dobataa <dobatabg04@gmail.com>
2024-03-20 00:07:43 +0200

    auth middleware update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,21 @@
+const cookieName = 'USER_SESSION';
+const jwt = require('jsonwebtoken');
+const { SECRET } = require('../config/config');
+
 module.exports = function () {
     return (req, res, next) => {
+        let token = req.cookies[cookieName];
+        if (token) {
+            jwt.verify(token, SECRET, function (err, decoded) {
+                if (err) {
+                    res.clearCookie(cookieName);
+                } else {
+                    req.user = decoded;
+                }
+            });
+
+        }
+
         next();
     };
 }
